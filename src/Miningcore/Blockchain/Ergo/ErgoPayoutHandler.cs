694e5d71e728994aa74f0422a2fde2a7493bd21f
Kirat-S <kiratgsingh@gmail.com>
2022-01-26 19:33:55 -0500

    Changed Ergo Confirmation number to 360

--- src/Miningcore/Blockchain/Ergo/ErgoPayoutHandler.cs
+++ src/Miningcore/Blockchain/Ergo/ErgoPayoutHandler.cs
@@ -138,7 +138,7 @@ namespace Miningcore.Blockchain.Ergo
             var pageSize = 100;
             var pageCount = (int) Math.Ceiling(blocks.Length / (double) pageSize);
             var result = new List<Block>();
-            var minConfirmations = extraPoolPaymentProcessingConfig?.MinimumConfirmations ?? (network == "mainnet" ? 720 : 72);
+            var minConfirmations = extraPoolPaymentProcessingConfig?.MinimumConfirmations ?? (network == "mainnet" ? 360 : 72);
             var minerRewardsPubKey = await ergoClient.MiningReadMinerRewardPubkeyAsync(ct);
             var minerRewardsAddress = await ergoClient.MiningReadMinerRewardAddressAsync(ct);
 
